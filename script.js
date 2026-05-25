78f286fdbde7912bae2d968fb3dbc88cba43a8a3
kartikbh01 <kartik.bh@omnireach.com>
2026-05-25 15:13:01 +0530

    initial setup

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+// Tabs
+const stopwatchTab = document.querySelector("#stopwatch-tab");
+const timerTab = document.querySelector("#timer-tab");
+const stopwatchSection = document.querySelector("#stopwatch-section");
+const timerSection = document.querySelector("#timer-section");
+// Stopwatch
+const stopwatchDisplay = document.querySelector("#stopwatch-display");
+const swStartBtn = document.querySelector("#sw-start");
+const swPauseBtn = document.querySelector("#sw-pause");
+const swResetBtn = document.querySelector("#sw-reset");
+// Timer
+const minutesInput = document.querySelector("#minutes-input");
+const secondsInput = document.querySelector("#seconds-input");
+const timerDisplay = document.querySelector("#timer-display");
+const timerStartBtn = document.querySelector("#timer-start");
+const timerPauseBtn = document.querySelector("#timer-pause");
+const timerResetBtn = document.querySelector("#timer-reset");
+// features to implement
+// 1. Tab switching
+// 2. Stopwatch start
+// 3. Stopwatch pause
+// 4. Stopwatch reset
+// 5. Timer start
+// 6. Timer pause
+// 7. Timer reset
+// 8. Update displays
+// 9. Input validation
---
+++ script.ts
@@ -0,0 +1,70 @@
+// Tabs
+const stopwatchTab = document.querySelector(
+  "#stopwatch-tab"
+) as HTMLButtonElement;
+
+const timerTab = document.querySelector(
+  "#timer-tab"
+) as HTMLButtonElement;
+
+const stopwatchSection = document.querySelector(
+  "#stopwatch-section"
+) as HTMLElement;
+
+const timerSection = document.querySelector(
+  "#timer-section"
+) as HTMLElement;
+
+// Stopwatch
+const stopwatchDisplay = document.querySelector(
+  "#stopwatch-display"
+) as HTMLDivElement;
+
+const swStartBtn = document.querySelector(
+  "#sw-start"
+) as HTMLButtonElement;
+
+const swPauseBtn = document.querySelector(
+  "#sw-pause"
+) as HTMLButtonElement;
+
+const swResetBtn = document.querySelector(
+  "#sw-reset"
+) as HTMLButtonElement;
+
+// Timer
+const minutesInput = document.querySelector(
+  "#minutes-input"
+) as HTMLInputElement;
+
+const secondsInput = document.querySelector(
+  "#seconds-input"
+) as HTMLInputElement;
+
+const timerDisplay = document.querySelector(
+  "#timer-display"
+) as HTMLDivElement;
+
+const timerStartBtn = document.querySelector(
+  "#timer-start"
+) as HTMLButtonElement;
+
+const timerPauseBtn = document.querySelector(
+  "#timer-pause"
+) as HTMLButtonElement;
+
+const timerResetBtn = document.querySelector(
+  "#timer-reset"
+) as HTMLButtonElement;
+
+
+// features to implement
+// 1. Tab switching
+// 2. Stopwatch start
+// 3. Stopwatch pause
+// 4. Stopwatch reset
+// 5. Timer start
+// 6. Timer pause
+// 7. Timer reset
+// 8. Update displays
+// 9. Input validation
